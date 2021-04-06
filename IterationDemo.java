980edea449acfdd847c65abe7f1d3c9369931b18
animesdas2013 <47323996+animesdas2013@users.noreply.github.com>
2021-04-07 00:05:13 +0530

    Update IterationDemo.java

--- IterationDemo.java
+++ IterationDemo.java
@@ -8,6 +8,19 @@ public class IterationDemo {
 			}
 			System.out.println(" ");
 		}
+		System.out.println();
+
+		for (int i = 0; i <= 5; i++) {
+			for (int j = 0; j <= 5; j++) {
+				{
+					if (i == 0 || i == 5 || j == 0 || j == 5)
+						System.out.print(" *");
+					else
+						System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
 
 	}
 
